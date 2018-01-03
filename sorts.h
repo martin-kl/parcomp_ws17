586f92ced61ca5e6618f2c5e0b3e3b8a6962a737
martin <e1526110@student.tuwien.ac.at>
2018-01-03 20:11:18 +0100

    added sorts file

---
+++ sorts.h
@@ -0,0 +1,19 @@
+/* Definition of sequential quicksort.
+ * We need this cause we call this algorithm when we are lower than a unit value
+ * to sort sequential.
+ *
+ * arr[] --> Array to be sorted,
+ * low  --> Starting index,
+ * high  --> Ending index (attention, high = sizeOfArray-1)
+ * */
+void quicksortS(int arr[], int low, int high);
+
+
+//Definition of openmp implementation
+void quicksortO(int arr[], int n, int maxThreads);
+
+//Definition of cilk implementation
+void quicksortC(int arr[], int n, int maxThreads);
+
+//Definition of mpi implementation
+void quicksortM(int arr[], int n, int maxThreads);
